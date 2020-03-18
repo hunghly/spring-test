44ad8c8f63deab2c8c5f708a12563f5bad6b42b5
Hung Ly <hunghly@gmail.com>
2020-03-18 12:10:22 -0500

    worked more on products

--- src/main/java/com/codeup/springtest/controllers/ProductController.java
+++ src/main/java/com/codeup/springtest/controllers/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class ProductController {
 
@@ -28,9 +30,9 @@ public class ProductController {
         return "/products/product-page";
     }
 
-    @GetMapping("/products/delete/{id}")
+    @PostMapping("/products/delete/{id}")
     public String deleteProduct(@PathVariable long id) {
-        productDao.delete(productDao.getOne(id));
+        productDao.deleteById(id);
         return "redirect:/products";
     }
 
@@ -44,6 +46,32 @@ public class ProductController {
     public String doCreateProduct(@ModelAttribute Product product) {
         productDao.save(product);
         return "redirect:/products";
+    }
+
+    @GetMapping("/products/edit/{id}")
+    public String viewEditProduct(@PathVariable long id, Model view) {
+        Product product = productDao.getOne(id);
+        view.addAttribute("product", product);
+        return "/products/edit";
+    }
+
+    @PostMapping("/products/edit/{id}")
+    public String doEditProduct(@PathVariable long id, @ModelAttribute Product product, Model view) {
+        Product foundProduct = productDao.getOne(id);
+        foundProduct.setName(product.getName());
+        foundProduct.setDescription(product.getDescription());
+        foundProduct.setPriceInCents(product.getPriceInCents());
+        foundProduct.setInStock(product.isInStock());
+        productDao.save(foundProduct);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/search")
+    public String searchProduct(@RequestParam String query, Model view) {
+        System.out.println(query);
+        List<Product> productList = productDao.searchByNameOrDescription(query);
+        view.addAttribute("products", productList);
+        return "/products/index";
     }
 
 }
--- src/main/java/com/codeup/springtest/repositories/ProductRepository.java
+++ src/main/java/com/codeup/springtest/repositories/ProductRepository.java
@@ -2,7 +2,16 @@ package com.codeup.springtest.repositories;
 
 import com.codeup.springtest.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+//    List<Product> findProductByNameOrDescription(String name, String description);
+//
+//
+//    List<Product> findByNameOrDescriptionContaining(String name, String description);
 
+    @Query("select p from Product p where p.name like %?1% or p.description like %?1%")
+    List<Product> searchByNameOrDescription(String query);
 }
